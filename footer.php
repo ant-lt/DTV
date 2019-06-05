26efe1621e002d091773cca5691e73e23e0853cc
proxybird <grainys@gmail.com>
2019-06-05 20:49:41 +0300

    sutvarkyti headeris ir footeris

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<p>All rights reserved by DTV 2019</p>
+<p class="centered">All rights reserved by DTV 2019</p>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
