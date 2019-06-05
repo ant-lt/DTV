47a0225e1744af0ac1b4cfb24338b3ec2b100cf4
proxybird <grainys@gmail.com>
2019-06-05 19:21:17 +0300

    sutvarkyti headeris ir footeris

--- bendras.php
+++ bendras.php
@@ -2,10 +2,28 @@
 <html>
 <head>
 	<title>Apie šalis trumpai</title>
-	<?php 'header.php'?>
+	<?php include 'header.php';?>
 </head>
 <body>
+	<div class="tg_konteineris">
+		<div class="tg_deze">
+			<h1 class="tg_centras">Indija</h1>
+			<img src="https://picsum.photos/150/150">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+			<div class="tg_deze">
+			<h1 class="tg_centras">Tailandas</h1>
+			<img src="https://picsum.photos/150/150">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+			<div class="tg_deze">
+			<h1 class="tg_centras">Kinija</h1>
+			<img src="https://picsum.photos/150/150">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+</div>
 
-<script type="text/javascript" src="js/materialize.min.js"></script>
+<?php include 'footer.php'; ?>
+
 </body>
 </html>
---
+++ footer.php
@@ -0,0 +1,4 @@
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script type="text/javascript" src="scripts/script.js"></script>
