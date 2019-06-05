9065f5d567061a2040701107c950bdd66db259f6
unknown <donatas@lotustravel.lt>
2019-06-05 20:45:35 +0300

    sukurtas backgroundas ir logo su nuoroda

--- menu.php
+++ menu.php
@@ -1,20 +1,28 @@
-		<ul id="dropdown1" class="dropdown-content">
-  			<li><a href="sablonas.php">testas</a></li>
-  			<li><a href="#!">two</a></li>
-  <li class="divider"></li>
-  <li><a href="#!">three</a></li>
+		
+
+<ul id="dropdown1" class="dropdown-content">
+ <li><a href="sablonas.php">testas</a></li>
+ <li><a href="#!">two</a></li>
+ <li class="divider"></li>
+ <li><a href="#!">three</a></li>
 </ul>
 <nav>
   <div class="nav-wrapper">
-    <a href="#!" class="brand-logo">Logo</a>
-    <ul class="right hide-on-med-and-down">
-      <li><a href="pagrindinis.php">Pagrindinis</a></li>
-      <li><a href="apie.php">Apie mus</a></li>
-      <!-- Dropdown Trigger -->
-      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Šalys<i class="material-icons right">arrow_drop_down</i></a></li>
-    </ul>
-  </div>
+   <img class="brand-logo" src="images/logo2.jpg"><a href="https://www.lotustravel.lt" class="brand-logo"></a>
+   <ul class="right hide-on-med-and-down">
+    <li><a href="pagrindinis.php">Pagrindinis</a></li>
+    <li><a href="apie.php">Apie mus</a></li>
+    <!-- Dropdown Trigger -->
+    <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Šalys<i class="material-icons right">arrow_drop_down</i></a></li>
+  </ul>
+</div>
 </nav>
-        
+<div class="dk_background">
+  <div class="dk_background2">
+    
+  </div>
+</div>
 
-		</div>
+
+
+</div>
