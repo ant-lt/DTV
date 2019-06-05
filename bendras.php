499f657b4cfe68172b03f87a4391ddf7b935deae
proxybird <grainys@gmail.com>
2019-06-05 21:35:05 +0300

    daugmaz aptvarkyta bendras.php

--- bendras.php
+++ bendras.php
@@ -10,24 +10,46 @@
 <body>
 	<?php include "menu.php"; ?>
 	<div class="container">
-		<div class="row">
-			<div class="col s12 m12 l12 xl12 white center-align"><span class="flow-text">
-			<h1>Indija</h1>
-			<img class="responsive-img materialboxed" src="https://picsum.photos/200">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		<div class="row white tg-remelis">
+			<div class="col s12 l4 ">
+				<img class="responsive-img materialboxed tg-nuotrauka" src="https://picsum.photos/200">
 			</div>
+			<div class="col s12 l8 ">
+				<h3 class="center-align">Indija</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<a href="sablonas.php">daugiau</a>
 			</div>
-			<div class="tg_deze">
-			<h1 class="tg_centras">Tailandas</h1>
-			<img src="https://picsum.photos/150/150">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+
+		<div class="row white tg-remelis">
+			<div class="col s12 l4 ">
+				<img class="responsive-img materialboxed tg-nuotrauka" src="https://picsum.photos/200">
 			</div>
-			<div class="tg_deze">
-			<h1 class="tg_centras">Kinija</h1>
-			<img src="https://picsum.photos/150/150">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<div class="col s12 l8 ">
+				<h3 class="center-align">Tailandas</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<a href="sablonas.php">daugiau</a>
 			</div>
+		</div>
+
+		<div class="row white tg-remelis">
+			<div class="col s12 l4 ">
+				<img class="responsive-img materialboxed tg-nuotrauka" src="https://picsum.photos/200">
+			</div>
+			<div class="col s12 l8 ">
+				<h3 class="center-align">Kinija</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<a href="sablonas.php">daugiau</a>
+			</div>
+		</div>
+
+			
+			
+			
 </div>
+			
+
+
 
 <?php include 'footer.php'; ?>
 
