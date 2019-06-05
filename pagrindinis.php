08b2dfbd76cd0676847d619ac6af975e601eac9d
vitagruz <v.gruzdas74@gmail.com>
2019-06-05 17:05:02 +0300

    meniu ir linku idejimas

--- pagrindinis.php
+++ pagrindinis.php
@@ -8,6 +8,20 @@
 
 <?php include "header.php"; ?>
 
+		<div class="header-top">
+				<a href="#">
+					<img class="logo-img" src="images/logo.png">
+				</a>
+
+				<nav>
+					<ul>
+						<li><a href="pagrindinis.php">Pagrindinis</a></li>
+						<li><a href="sablonas.php">Šalys</a></li>
+						<li><a href="apie.php">Apie mus</a></li>
+					</ul>
+				</nav>
+		</div>
+
 	
 	<script type="text/javascript" src="js/materialize.min.js"></script>
 	<?php include "footer.php"; ?>
