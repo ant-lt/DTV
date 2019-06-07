99923b99b2e47e6fd050502ad69638209c6c9989
grainys@gmail.com <grainys@gmail.com>
2019-06-07 18:39:19 +0300

    ieskau klaidu pas save

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-
+<div class="reliatyvus">
 <p class="center-align">All rights reserved by DTV 2019</p>
-
+</div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 <script type="text/javascript" src="scripts/script.js"></script>
